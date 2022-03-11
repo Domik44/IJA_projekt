72b9fa10ee97a7da5ec0982d8f71cfbd843db3bb
Domik44 <80786120+Domik44@users.noreply.github.com>
2022-03-11 17:29:12 +0100

    Create_uml_packlage
    Created uml package + first Element class

---
+++ src/uml/Element.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package uml;
+
+/**
+ * @author xpopdo00
+ *
+ */
+public class Element {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Čau");
+
+	}
+
+}
