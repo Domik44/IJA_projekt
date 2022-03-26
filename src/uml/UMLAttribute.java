33909f240f17e67ba5dafd136a42c06b323d67b8
Domik44 <80786120+Domik44@users.noreply.github.com>
2022-03-27 00:17:38 +0100

    Update_UMLAttribute
    Changed toString, constructor. Added visbility.

--- src/uml/UMLAttribute.java
+++ src/uml/UMLAttribute.java
@@ -1,14 +1,32 @@
+/**
+* <h1>UMLAttribute</h1>
+* This class represents attribute
+* of class/operation in class diagrams.
+*
+* @author  Dominik Pop
+* @version 1.0
+* @since   2022-03-23 
+*/
+
+
 package uml;
 
 public class UMLAttribute extends Element {
 
 	// Attributes
 	private UMLClassifier type;
+	private UMLClassifier visibility;
 	
 	// Constructors
-	public UMLAttribute(java.lang.String name, UMLClassifier type) {
+	public UMLAttribute(String name, UMLClassifier type) {
 		super(name);
 		this.type = type;
+	}
+	
+	public UMLAttribute(String name, UMLClassifier type, UMLClassifier visibility) {
+		super(name);
+		this.type = type;
+		this.visibility = visibility;
 	}
 	
 	// Methods
@@ -16,9 +34,20 @@ public class UMLAttribute extends Element {
 		return this.type;
 	}
 	
+	public UMLClassifier getVisibility() {
+		return this.visibility;
+	}
+	
 	@Override
-	public java.lang.String toString(){
-		java.lang.String result = this.getName() + ":" + this.type;
+	public String toString(){
+		UMLClassifier vis = this.getVisibility();
+		String result;
+		if(vis == null) {
+			result = this.getName() + ": " + this.type;
+		}
+		else {			
+			result = this.getVisibility() + " " + this.getName() + ": " + this.type;
+		}
 		
 		return result;
 	}
