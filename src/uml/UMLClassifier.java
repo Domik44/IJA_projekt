1b84c038596319debfe464c99d1a514b0c678707
Domik44 <80786120+Domik44@users.noreply.github.com>
2022-03-27 00:16:26 +0100

    Update_UMLClassifier

--- src/uml/UMLClassifier.java
+++ src/uml/UMLClassifier.java
@@ -1,3 +1,13 @@
+/**
+* <h1>UMLAttribute</h1>
+* This class represents classifier
+* in class diagrams.
+*
+* @author  Dominik Pop
+* @version 1.0
+* @since   2022-03-23 
+*/
+
 package uml;
 
 public class UMLClassifier extends Element {
@@ -6,19 +16,19 @@ public class UMLClassifier extends Element {
 	private boolean userDefined;
 
 	// Constructors
-	public UMLClassifier(java.lang.String name) {
+	public UMLClassifier(String name) {
 		super(name);
-		this.userDefined = true;
+		this.userDefined = false; // Tady zmena oproti ukolu 1
 	}
 	
-	public UMLClassifier(java.lang.String name, boolean isUserDefined) {
+	public UMLClassifier(String name, boolean isUserDefined) {
 		super(name);
 		this.userDefined = isUserDefined;
 	}
 
 	// Methods
-	public static UMLClassifier forName(java.lang.String name) {
-		return new UMLClassifier(name, false);
+	public static UMLClassifier forName(String name) {
+		return new UMLClassifier(name, true); // Tady zmena oproti ukolu 1
 	}
 	
 	public boolean isUserDefined() {
@@ -26,8 +36,14 @@ public class UMLClassifier extends Element {
 	}
 	
 	@Override
-	public java.lang.String toString(){
-		java.lang.String result = this.getName() + '(' + this.isUserDefined() +  ')';
+	public String toString(){
+		// TODO changed this -> maybe change back for future use?
+		//java.lang.String result = this.getName() + '(' + this.isUserDefined() +  ')';
+		String result = this.getName();
+		
+		if(result.equals("null")) {
+			result = "";
+		}
 		
 		return result;
 	}
