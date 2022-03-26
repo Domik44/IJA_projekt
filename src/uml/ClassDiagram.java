e4ebf4313db078812890fae18e4995dcbfac9d24
Domik44 <80786120+Domik44@users.noreply.github.com>
2022-03-27 00:18:30 +0100

    Update_ClassDiagram

--- src/uml/ClassDiagram.java
+++ src/uml/ClassDiagram.java
@@ -1,48 +1,140 @@
+/**
+* <h1>ClassDiagram</h1>
+* This class represents class diagram
+* as one piece.
+*
+* @author  Dominik Pop
+* @version 1.0
+* @since   2022-03-23 
+*/
+
 package uml;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClassDiagram extends Element {
 	// Attributes
-	private java.util.List<UMLClassifier> classes = new java.util.ArrayList<UMLClassifier>();
+	private List<UMLInterface> interfaces = new ArrayList<UMLInterface>();
+	private List<UMLClass> classes = new ArrayList<UMLClass>();
+	private List<UMLRelation> relations = new ArrayList<UMLRelation>();
+	private List<UMLClassifier> types = new ArrayList<UMLClassifier>();
 
 	// Constructors
-	public ClassDiagram(java.lang.String name) {
+	public ClassDiagram(String name) {
 		super(name);
 	}
 	
 	// Methods
-	// Creates new class
-	public UMLClass createClass(java.lang.String name) { // TO DO
-		UMLClass newClass = new UMLClass(name);
-		this.classes.add(newClass);
-		
-		return newClass;
-		
-	}
 	
-	// Tries to find classifier, if it doesnt exist it creates new one
-	public UMLClassifier classifierForName(java.lang.String name) {	 // TO DO
+	// Tries to find classifier, if it doesn't exist it creates new one
+	public UMLClassifier classifierForName(String name) {	 // TO DO
 		
 		UMLClassifier res = this.findClassifier(name);
 		if(res != null)
 			return res;
 		
 		UMLClassifier result = UMLClassifier.forName(name);
-		this.classes.add(result);
-		return result;
+		this.types.add(result);
 		
+		return result;
 	}
 	
 	// In case of success returns Classifier, otherwise null
-	public UMLClassifier findClassifier(java.lang.String name) { // TO DO
+	public UMLClassifier findClassifier(String name) { // TO DO
 		
-		for (UMLClassifier umlClass : this.classes) {
-			if(umlClass.getName() == name) {
-				return umlClass;
+		for (UMLClassifier classifier : this.types) {
+			if(classifier.getName() == name) {
+				return classifier;
 			}
 		}
 		
 		return null;
-
+	}
+	
+	// Creates basic data types and adds them to types list
+	public void basicTypes() {
+		String[] basices = {"int", "str", "bool", "float", "+", "-", "#", "~"};
+		for(String str : basices) {
+			UMLClassifier obj = new UMLClassifier(str);
+			this.types.add(obj);
+		}
+	}
+	
+	// Creates interface and adds it to interface list
+	public UMLInterface createInterface(String name) { // TO DO
+		UMLInterface newInterface = new UMLInterface(name);
+		this.interfaces.add(newInterface);		
+		
+		return newInterface;	
+	}
+	
+	
+	public List<UMLInterface> getInterfaces(){
+		List<UMLInterface> copyList = List.copyOf(this.interfaces);
+		return copyList;
+	}
+	
+	public UMLInterface getInterface(String name) {
+		for(UMLInterface in : this.interfaces) {
+			if(in.getName().equals(name)) {
+				return in;
+			}
+		}
+		
+		return null;
+	}
+	
+	// Creates class and adds it to class list
+	public UMLClass createClass(String name) { // TO DO
+		UMLClass newClass = new UMLClass(name);
+		this.classes.add(newClass);		
+		
+		return newClass;	
+	}
+	
+	
+	public List<UMLClass> getClasses(){
+		List<UMLClass> copyList = List.copyOf(this.classes);
+		return copyList;
+	}
+	
+	public UMLClass getClass(String name) {
+		for(UMLClass cl : this.classes) {
+			if(cl.getName().equals(name)) {
+				return cl;
+			}
+		}
+		
+		return null;
+	}
+	
+	// Creates relation and adds it to relation list
+	public RelGeneralization createGeneralization(UMLInterface parent, UMLInterface child, String type) {
+		RelGeneralization newRelation = new RelGeneralization(parent, child, type);
+		this.relations.add(newRelation);
+		
+		return newRelation;
+	}
+	
+	public RelAggregation createAggregation(UMLInterface parent, UMLInterface child, String type) {
+		RelAggregation newRelation = new RelAggregation(parent, child, type);
+		this.relations.add(newRelation);
+		
+		return newRelation;
+	}
+	
+	public RelAssociation createAssociation(UMLInterface parent, UMLInterface child, String type) {
+		RelAssociation newRelation = new RelAssociation(parent, child, type);
+		this.relations.add(newRelation);
+		
+		return newRelation;
+	}
+	
+	//
+	public List<UMLRelation> getRelations(){
+		List<UMLRelation> copyList = List.copyOf(this.relations);
+		return copyList;
 	}
 
 }
