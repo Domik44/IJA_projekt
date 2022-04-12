295be45a4f10968045390b0c050399b0904d3533
Domik44 <xpopdo00@stud.fit.vutbr.cz>
2022-04-12 15:48:46 +0200

    Update_UMLRelation

--- src/uml/UMLRelation.java
+++ src/uml/UMLRelation.java
@@ -14,10 +14,14 @@
 
 package uml;
 
+import java.util.List;
+import uml.pos.Position;
+
 public class UMLRelation{
 	private UMLInterface leftClass;
 	private UMLInterface rightClass;
 	private String type;
+	private List<Position> listPoints = new java.util.ArrayList<Position>();
 	
 	public UMLRelation(UMLInterface left, UMLInterface right, String type) {
 		this.type = type;
@@ -37,5 +41,18 @@ public class UMLRelation{
 	// TODO  -> dodelat getType -> popremylet jestli ho nedelat pres Classifier
 	public String getType() {
 		return this.type;
+	}
+	
+	public void addPosition(Position pos) {
+		listPoints.add(pos);
+	}
+	
+	public List<Position> getPoints() {
+		return this.listPoints;
+	}
+	
+	public void changeList(List<Position> newList) {
+		this.listPoints = null;
+		this.listPoints = newList;
 	}
 }
